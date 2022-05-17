fc9a07fd7356ae2e895e3a18455c88ee8bd8fe88
XAV1KH4N <xavierkhan31@gmail.com>
2022-05-17 11:51:13 +0100

    Created Cars Execute function

--- Car.py
+++ Car.py
@@ -2,7 +2,31 @@ class Car:
     def __init__(self, left, right):
         self.left = left
         self.right = right
+        self.SPEED = 1560
         
     def execute(buttons):
-        pass
+                
+        if Button.LEFT_UP in buttons:
+            self.left.run(self.SPEED)
+            
+        elif Button.LEFT_DOWN in buttons:
+            self.left.run(-self.SPEED)
+
+        elif Button.LEFT_UP not in buttons and Button.LEFT_DOWN not in buttons:
+            self.left.brake()
+
+
+        if Button.RIGHT_UP in buttons:
+            self.right.run(self.SPEED)
+        
+        elif Button.RIGHT_DOWN in buttons:
+            self.right.run(-self.SPEED)        
+
+        elif Button.RIGHT_UP not in buttons and Button.RIGHT_DOWN not in buttons:
+            self.right.brake()
+            
+        return Button.CENTER not in ev3.buttons.pressed()
+    
+
+
         
