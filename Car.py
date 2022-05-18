551c8da1231c9a47296d999fed5119020bc8249e
XAV1KH4N <xavierkhan31@gmail.com>
2022-05-18 14:46:18 +0100

    Refactored cars execute function

--- Car.py
+++ Car.py
@@ -10,44 +10,51 @@ class Car:
         
     def execute(self,buttons):
         if Button.LEFT_DOWN in buttons and Button.LEFT_UP in buttons:
-            self.left.brake()
-            self.right.brake()
-                               
-            if Button.RIGHT_UP in buttons:
-                self.arm.run(-self.SPEED)
-                
-            elif Button.RIGHT_DOWN in buttons:
-                self.arm.run(self.SPEED)
-                
-            else:
-                self.arm.brake()
-                
+            self.arm_motor(buttons)
+
+                                
         else:    
-            if Button.LEFT_DOWN in buttons:
-                self.left.run(-self.SPEED)
-                
-            if Button.LEFT_UP in buttons:
-                self.left.run(self.SPEED)
-                
-            elif Button.LEFT_DOWN in buttons:
-                self.left.run(-self.SPEED)
-
-            elif Button.LEFT_UP not in buttons and Button.LEFT_DOWN not in buttons:
-                self.left.brake()
-                self.arm.brake()
-                
-
-            if Button.RIGHT_UP in buttons:
-                self.right.run(self.SPEED)
-            
-            elif Button.RIGHT_DOWN in buttons:
-                self.right.run(-self.SPEED)        
-
-            elif Button.RIGHT_UP not in buttons and Button.RIGHT_DOWN not in buttons:
-                self.right.brake()
+            self.left_motor(buttons)
+            self.right_motor(buttons)
             
         return Button.CENTER not in self.ev3.buttons.pressed()
     
+    def right_motor(self, buttons):
+        if Button.RIGHT_UP in buttons:
+            self.right.run(self.SPEED)
+            
+        elif Button.RIGHT_DOWN in buttons:
+            self.right.run(-self.SPEED)        
 
+        elif Button.RIGHT_UP not in buttons and Button.RIGHT_DOWN not in buttons:
+            self.right.brake()
+    
+    def left_motor(self, buttons):
+        if Button.LEFT_DOWN in buttons:
+                self.left.run(-self.SPEED)
+                
+        if Button.LEFT_UP in buttons:
+            self.left.run(self.SPEED)
+                
+        elif Button.LEFT_DOWN in buttons:
+            self.left.run(-self.SPEED)
+
+        elif Button.LEFT_UP not in buttons and Button.LEFT_DOWN not in buttons:
+            self.left.brake()
+            self.arm.brake()
+    
+    
+    def arm_motor(self, buttons):
+        self.left.brake()
+        self.right.brake()
+                               
+        if Button.RIGHT_UP in buttons:
+            self.arm.run(-self.SPEED)
+                
+        elif Button.RIGHT_DOWN in buttons:
+            self.arm.run(self.SPEED)
+                
+        else:
+            self.arm.brake()
 
         
